1d4fb102b005a2f5b3b1550a4ba82f0b9a29ff62
Deraj103 <jstims9109@gmail.com>
2024-02-01 20:49:01 -0700

    Add project files.

---
+++ InventoryClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Module_3_Competency_Project
+{
+    internal class InventoryClass
+    {
+        double itemID;
+        string itemName;
+        double itemPrice;
+        double itemQuantity;
+        string reply;
+
+        public InventoryClass(double i, string n, double p, double q)
+        {
+            this.itemID = i;
+            this.itemName = n;
+            this.itemPrice = p;
+            this.itemQuantity = q;
+        }
+
+        public void displayItem()
+        {
+            WriteLine($"Item {itemID} {itemName}, {itemPrice:C}, quantity: {itemQuantity}");
+        }
+
+        public void purchase()
+        {
+            double qtyReq;
+            double purchaseCost;
+
+            Write("How many do you want?");
+            qtyReq = double.Parse(ReadLine());
+            do
+            {
+                if (itemQuantity < qtyReq)
+                {
+                    WriteLine("There is not enough quantity of that item. Please request a different amount.");
+                }
+                else
+                {
+                    itemQuantity -= qtyReq;
+                    purchaseCost = qtyReq * itemPrice;
+                    WriteLine($"{purchaseCost:C}");
+                }
+
+                WriteLine("\nWould you like anything else? Yes(Y) or No(N)");
+                reply = ReadLine();
+            } while (reply == "Y");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Module_3_Competency_Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Module_3_Competency_Project_Purchases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Module 3 Competency Project: Purchases by Jared Tims\n");
+
+            // item instantiation
+            InventoryClass item1 = new InventoryClass(123, "soda", 1.19, 50);
+            InventoryClass item2 = new InventoryClass(987, "cookie", 1.50, 20);
+
+            item1.displayItem();
+            item2.displayItem();
+
+            item1.purchase();
+            item2.purchase();
+
+            Write("\nPress any key to continue...");
+            ReadKey();
+        }
+    }
+}
